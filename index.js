033b151051a863aaa4007147f2de0c6bdb0d6fba
Victoria <vitalina27@gmail.com>
2017-11-06 13:49:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function updateObjectWithKeyAndValue(object, key, value){
 
-  var newObj = Object.assign({}, obj,key:value);
+var newObj = Object.assign({}, obj,{key:value});
+//object.key=value;
   return newObj;
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object.key=value;
+    return object;
+  
 }
