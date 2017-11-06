20b36dfac638968d8384b1ce083db74614a1b29d
Victoria <vitalina27@gmail.com>
2017-11-06 13:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
 delete object.key;
 return object;
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object.key;
   return object;
-  
+
 }
