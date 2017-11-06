49aceb350e9fe275d1b9fcdbb0ef66680b63258d
Victoria <vitalina27@gmail.com>
2017-11-06 13:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
   var obj = { prop: 1 };
   function updateObjectWithKeyAndValue(obj, key, value){
 
-var newObj = Object.assign({}, obj,{key:value});
-//object.key=value;
-  return newObj;
+//var newObj = Object.assign({}, obj,{key:value});
+obj.key=value;
+  return obj;
 }
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   object.key=value;
